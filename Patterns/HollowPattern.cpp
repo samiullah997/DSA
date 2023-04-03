6f0eb20d0594a35b7e12e6d65a94eeb1ea603c13
samiullah997 <samiullahk997@gmail.com>
2023-04-03 19:48:22 +0500

    Solve Hollow Pattern Challenge

---
+++ Patterns/HollowPattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int rows=4;
+    int columns=5;
+    for(int i = 0; i <= rows; i++){
+        for(int j = 0; j <= columns; j++){
+            if(i == 0 || i == rows || j == 0 || j == columns){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
