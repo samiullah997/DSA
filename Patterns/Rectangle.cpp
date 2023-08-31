ba8bcabc67618befd10d1224e3da223dc0f3c005
Samiullah <samiullahk997@gmail.com>
2023-08-31 13:26:24 +0500

    added more spaces to improve pattern.

--- Patterns/Rectangle.cpp
+++ Patterns/Rectangle.cpp
@@ -7,8 +7,8 @@ int main(){
     int columns=5;
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < columns; j++){
-            cout<<"*";
+            cout<<"* ";
         }
-        cout<<"*"<<endl;
+        cout<<"* "<<endl;
     }
 }
