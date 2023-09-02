8c00a22a7fb14c501e57b8f13aa615077e8a2941
Samiullah <samiullahk997@gmail.com>
2023-09-02 13:31:42 +0500

    added more spaces to improve pattern.

--- Patterns/ZeroOnePattern.cpp
+++ Patterns/ZeroOnePattern.cpp
@@ -7,9 +7,9 @@ int main(){
     for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++){ 
             if((i+j)%2 == 0){
-                cout<<0;
+                cout<<0<<" ";
             }else {
-                cout<<1;
+                cout<<1<<" ";
             }
         }
         cout<<endl;
