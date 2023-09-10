a8709ebd245758db188d1ccb514b494d9e57af57
Samiullah <samiullahk997@gmail.com>
2023-09-10 14:37:19 +0500

    added comments to explain the project.

--- Patterns/FloyidTringle.cpp
+++ Patterns/FloyidTringle.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int n = 5;
     int count = 1;
-    for(int i=1;i<=n;i++){
+    for(int i=1;i<=n;i++){ // start loop from 1 to n
         for(int j=1;j<=i;j++){
             cout<<count<<"  "; // print count number and spaces
             count++; // increment counter
