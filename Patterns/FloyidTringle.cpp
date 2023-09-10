fce3aa76b00f2e8023e4498220e34f224679b168
Samiullah <samiullahk997@gmail.com>
2023-09-10 14:35:50 +0500

    added comments to explain the project.

--- Patterns/FloyidTringle.cpp
+++ Patterns/FloyidTringle.cpp
@@ -8,7 +8,7 @@ int main(){
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
             cout<<count<<"  "; // print count number and spaces
-            count++;
+            count++; // increment counter
         }
         cout<<endl;
     }
