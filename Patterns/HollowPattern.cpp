caa090732dce64f40bd200b5f5be82c9f58fb886
Samiullah <samiullahk997@gmail.com>
2023-09-13 14:40:28 +0500

    added comments to explain the project.

--- Patterns/HollowPattern.cpp
+++ Patterns/HollowPattern.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int rows=4;
     int columns=5;
-    for(int i = 0; i <= rows; i++){
+    for(int i = 0; i <= rows; i++){ // start 1st loop from 0 to rows
         for(int j = 0; j <= columns; j++){
             if(i == 0 || i == rows || j == 0 || j == columns){
                 cout<<"*  "; // print * with spaces
