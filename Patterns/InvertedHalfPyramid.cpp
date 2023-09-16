aae3142939367ae863f367f7863501b724fd6ee9
Samiullah <samiullahk997@gmail.com>
2023-09-16 14:42:56 +0500

    added comments to explain the project.

--- Patterns/InvertedHalfPyramid.cpp
+++ Patterns/InvertedHalfPyramid.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     int n=5;
-    for(int i=0;i<=n;i++){ 
+    for(int i=0;i<=n;i++){ // start 1st loop from 0 to n
         for(int j=i;j<=n;j++){
             cout<<"*  "; // print * with spaces
         }
