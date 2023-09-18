8ad7525b455161eb5071235b4ea1130a66f6e0b0
Samiullah <samiullahk997@gmail.com>
2023-09-18 14:44:14 +0500

    added comments to explain the project.

--- Patterns/InvertedHalfPyramidUsingNumber.cpp
+++ Patterns/InvertedHalfPyramidUsingNumber.cpp
@@ -6,7 +6,7 @@ int main(){
     int n=5;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
-            cout<<i<<"  ";
+            cout<<i<<"  "; // print i with spaces
         }
         cout<<endl;
     }
