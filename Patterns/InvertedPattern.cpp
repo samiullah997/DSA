f1128efbfac7750cd6c4b1a7c51e5d55706fcf7e
Samiullah <samiullahk997@gmail.com>
2023-09-22 14:47:47 +0500

    added comments to explain the code.

--- Patterns/InvertedPattern.cpp
+++ Patterns/InvertedPattern.cpp
@@ -6,7 +6,7 @@ int main(){
     int n=5;
     for(int i=0; i<=n; i++){
         for(int j=1; j<=n-i; j++){
-            cout<<j<<"  ";
+            cout<<j<<"  "; // print j with spaces
         }
         cout<<endl;
     }
