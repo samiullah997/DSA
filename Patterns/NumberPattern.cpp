75dacf872ca883cde86cb6f482b3f8e1b09dbe5d
Samiullah <samiullahk997@gmail.com>
2023-09-25 14:50:40 +0500

    added comments to explain the code.

--- Patterns/NumberPattern.cpp
+++ Patterns/NumberPattern.cpp
@@ -6,7 +6,7 @@ int main(){
     int n=5;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n-i;j++){
-            cout<<" ";
+            cout<<" "; // print spaces
         }
         for(int k=1;k<=i;k++){
             cout<<k<<" ";
