b7aed311d63adcdccb2d16a814b8ed456ac13539
Samiullah <samiullahk997@gmail.com>
2023-10-08 15:02:31 +0500

    added comments to explain the code.

--- Patterns/Rectangle.cpp
+++ Patterns/Rectangle.cpp
@@ -6,7 +6,7 @@ int main(){
     int rows=4;
     int columns=5;
     for(int i = 0; i < rows; i++){ // start 1st loop from 0 to rows
-        for(int j = 0; j < columns; j++){
+        for(int j = 0; j < columns; j++){ // start 2nd loop from 0 to columns
             cout<<"* ";
         }
         cout<<"* "<<endl;
