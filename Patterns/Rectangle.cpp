ca27df80df305fdd0485a826d2010c1359ca26a7
Samiullah <samiullahk997@gmail.com>
2023-10-09 15:03:00 +0500

    added comments to explain the code.

--- Patterns/Rectangle.cpp
+++ Patterns/Rectangle.cpp
@@ -7,7 +7,7 @@ int main(){
     int columns=5;
     for(int i = 0; i < rows; i++){ // start 1st loop from 0 to rows
         for(int j = 0; j < columns; j++){ // start 2nd loop from 0 to columns
-            cout<<"* ";
+            cout<<"* "; // print * with spaces
         }
         cout<<"* "<<endl;
     }
