f2a23faa77f5a32d9b4f4620df5ea8aebdccaaac
Samiullah <samiullahk997@gmail.com>
2023-10-11 15:04:33 +0500

    added comments to explain the code.

--- Patterns/RhombusPattern.cpp
+++ Patterns/RhombusPattern.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     int n=4;
-    for(int i=0;i<=n;i++){
+    for(int i=0;i<=n;i++){ // start 1st loop from 0 to n
         for(int j=n; j>=i; j--){
             cout<<" ";
         }
