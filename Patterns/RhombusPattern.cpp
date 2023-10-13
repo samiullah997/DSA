62f340652e0c59b83ee42b45e43f48527a0d50e7
Samiullah <samiullahk997@gmail.com>
2023-10-13 15:05:48 +0500

    added comments to explain the code.

--- Patterns/RhombusPattern.cpp
+++ Patterns/RhombusPattern.cpp
@@ -6,7 +6,7 @@ int main(){
     int n=4;
     for(int i=0;i<=n;i++){ // start 1st loop from 0 to n
         for(int j=n; j>=i; j--){ // start 2nd loop from n to i
-            cout<<" ";
+            cout<<" "; // print spaces
         }
         cout<<"*  *  *  *  *"<<endl;
     }
