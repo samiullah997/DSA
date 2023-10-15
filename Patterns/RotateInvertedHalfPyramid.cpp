9647d25d2bc483659bf923ad109897834e0a95c5
Samiullah <samiullahk997@gmail.com>
2023-10-15 15:18:38 +0500

    added comments to explain the code.

--- Patterns/RotateInvertedHalfPyramid.cpp
+++ Patterns/RotateInvertedHalfPyramid.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     int n=5;
-    for(int i=n;i>=1;i--){
+    for(int i=n;i>=1;i--){ // start 1st loop from n to 1
         for(int j=1;j<=n;j++){
             if(j>=i){
                 cout<<"*  ";
