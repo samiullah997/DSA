17ed7bdf88999298a175c384435d7801c097c85f
Samiullah <samiullahk997@gmail.com>
2023-10-16 15:19:34 +0500

    added comments to explain the code.

--- Patterns/StarPattern.cpp
+++ Patterns/StarPattern.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int n = 5;
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) // start 1st loop from 1 to n
     {
         for (int j = 1; j <= n - i; j++)
         {
