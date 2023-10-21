bbd1c00fc84c9a8782135c7607dee80a3802ee73
Samiullah <samiullahk997@gmail.com>
2023-10-21 15:22:52 +0500

    added comments to explain the code.

--- Patterns/ZeroOnePattern.cpp
+++ Patterns/ZeroOnePattern.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int n=5;
     for(int i=1; i<=n; i++){ // start 1st loop from 1 to n
-        for(int j=1; j<=i; j++){ 
+        for(int j=1; j<=i; j++){ // start 2nd loop from 1 to i
             if((i+j)%2 == 0){
                 cout<<0<<" ";
             }else {
