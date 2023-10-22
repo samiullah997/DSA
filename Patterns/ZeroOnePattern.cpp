0c3d61d0dd77fb365ea1dfcb75613bf0bd6fcd8b
Samiullah <samiullahk997@gmail.com>
2023-10-22 15:23:39 +0500

    added comments to explain the code.

--- Patterns/ZeroOnePattern.cpp
+++ Patterns/ZeroOnePattern.cpp
@@ -6,7 +6,7 @@ int main(){
     int n=5;
     for(int i=1; i<=n; i++){ // start 1st loop from 1 to n
         for(int j=1; j<=i; j++){ // start 2nd loop from 1 to i
-            if((i+j)%2 == 0){
+            if((i+j)%2 == 0){ // check condition if (i+j)%2 == 0
                 cout<<0<<" ";
             }else {
                 cout<<1<<" ";
