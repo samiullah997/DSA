47453381a6e4177037c5f4cb04e72328a173bf91
Samiullah <samiullahk997@gmail.com>
2023-10-26 15:32:08 +0500

    added comments to explain the code.

--- Patterns/ZigZagPattern.cpp
+++ Patterns/ZigZagPattern.cpp
@@ -6,7 +6,7 @@ int main(){
     int n=9;
     for(int i=1;i<=3;i++){ // start 1st loop from 1 to 3
         for(int j=1;j<=n;j++){ // start 2nd loop from 1 to n
-            if(((i+j)%4==0) || (i==2 && j%4==0)){
+            if(((i+j)%4==0) || (i==2 && j%4==0)){ // check condition if (i+j) % 4 == 0 OR i==2 AND j%4 == 0
                 cout<<"* ";
             }else{
                 cout<<"  ";
